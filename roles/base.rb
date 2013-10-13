a65d52dd744e83268c96f2dc7bf828847a921b0d
Reid Beels <mail@reidbeels.com>
2013-10-13 00:13:42 -0700

    Add openssh, disable root and password login

--- roles/base.rb
+++ roles/base.rb
@@ -2,4 +2,12 @@ name "base"
 run_list %W(
   recipe[apt]
   recipe[postfix]
+  recipe[openssh]
+)
+
+override_attributes(
+  'openssh' => {
+    'permit_root_login' => 'no',
+    'password_authentication' => 'no'
+  }
 )
