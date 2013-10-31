f5f12b7259feefd59a0e27b1f9bc4e84d4cc5d97
Reid Beels <mail@reidbeels.com>
2013-10-31 14:39:23 -0700

    Configure sysctl against spoofing and other attacks

--- roles/base.rb
+++ roles/base.rb
@@ -14,6 +14,7 @@ run_list %W(
   recipe[unattended-upgrades]
   recipe[ufw]
   recipe[shm_noexec]
+  recipe[sysctl]
 )
 
 default_attributes(
@@ -32,5 +33,47 @@ default_attributes(
   :firewall => {
     :rules => [
     ]
+  },
+  :sysctl => {
+    :params => {
+      :net => {
+        :ipv4 => {
+          :icmp_echo_ignore_broadcasts => 1,
+          :icmp_ignore_bogus_error_responses => 1,
+          :tcp_syncookies => 1,
+          :tcp_max_syn_backlog => 2048,
+          :tcp_synack_retries => 2,
+          :tcp_syn_retries => 5,
+          :conf => {
+            :all => {
+              :rp_filter => 1,
+              :accept_source_route => 0,
+              :send_redirects => 0,
+              :accept_redirects => 0,
+              :log_martians => 1,
+            },
+            :default => {
+              :rp_filter => 1,
+              :accept_source_route => 0,
+              :send_redirects => 0,
+              :accept_redirects => 0,
+              :log_martians => 1,
+            }
+          }
+        },
+        :ipv6 => {
+          :conf => {
+            :all => {
+              :accept_source_route => 0,
+              :accept_redirects => 0,
+            },
+            :default => {
+              :accept_source_route => 0,
+              :accept_redirects => 0,
+            }
+          }
+        }
+      }
+    }
   }
 )
