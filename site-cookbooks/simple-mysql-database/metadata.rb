f279ce4372dc20c597db522936df9e7c76b99334
Reid Beels <mail@reidbeels.com>
2014-02-13 02:14:38 -0800

    Add dependencies for simple-mysql-database cookbook

---
+++ site-cookbooks/simple-mysql-database/metadata.rb
@@ -0,0 +1,10 @@
+name             'simple-mysql-database'
+maintainer       'Reid Beels'
+maintainer_email 'reid@stumptownsyndicate.org'
+license          'MIT'
+description      'Creates MySQL databases based on data bags'
+version          '0.1.0'
+
+depends 'database'
+depends 'mysql'
+
