e63a5eb7f7b1a40da0e912f9248041fdc96c4f54
Reid Beels <mail@reidbeels.com>
2015-03-21 19:07:15 -0700

    Fix postgres setup for etherpad-lite

---
+++ site-cookbooks/etherpad-lite-db/metadata.rb
@@ -0,0 +1,11 @@
+name             'etherpad-lite-db'
+maintainer       'Reid Beels, Stumptown Syndicate'
+maintainer_email 'reid@stumptownsyndicate.org'
+license          'MIT'
+description      'Creates DB for etherpad lite'
+version          '0.1.0'
+
+depends 'nginx'
+depends 'database'
+depends 'postgresql'
+
--- site-cookbooks/etherpad-lite-db/recipes/default.rb
+++ site-cookbooks/etherpad-lite-db/recipes/default.rb
@@ -1,10 +1,18 @@
+postgres_connection_info = {
+  :host     => '127.0.0.1',
+  :username => 'postgres',
+  :password => node['postgresql']['password']['password']
+}
+
 postgresql_database_user 'etherpad' do
+  connection postgres_connection_info
   action   :create
   createdb true
   login    true
 end
 
 postgresql_database 'etherpad' do
+  connection postgres_connection_info
   owner 'etherpad'
 end
 
