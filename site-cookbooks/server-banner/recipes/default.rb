e9b7e80f1bd474b6085801bcbaaa872f0a079714
Reid Beels <reid@thedyrt.com>
2017-12-31 22:41:29 -0800

    Update server-banner to work with new apache2 security config

--- site-cookbooks/server-banner/recipes/default.rb
+++ site-cookbooks/server-banner/recipes/default.rb
@@ -2,17 +2,26 @@ name = node['server-banner']['domain'] || node['set_fqdn'] || node.name
 
 directory "/var/www/server-banner" do
   recursive true
+  owner 'www-data'
+  group 'admin'
+  mode '0755'
 end
 
 image = node['server-banner']['image']
 if image
   cookbook_file "/var/www/server-banner/#{image}" do
     source image
+    owner 'www-data'
+    group 'admin'
+    mode '0755'
   end
 end
 
 template "/var/www/server-banner/index.html" do
   source "index.html.erb"
+  owner 'www-data'
+  group 'admin'
+  mode '0755'
   variables(
     server_name: name,
     background_color: node['server-banner']['background_color'],
